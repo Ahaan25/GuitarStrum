85aa59be461d1368d231e7ce923075777d7babcd
Ahaan25 <ahaan25092002@gmail.com>
2022-04-12 17:40:05 +0530

    Hehe

--- app/src/main/java/com/example/guitarstrum/MainActivity.java
+++ app/src/main/java/com/example/guitarstrum/MainActivity.java
@@ -55,25 +55,3 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
-
-/*
-Widgets declared
-    public Button up=(Button) findViewById(R.id.up);
-    public Button down=(Button) findViewById(R.id.down);
-    public Button slap=(Button) findViewById(R.id.slap);
-    public Button clear=(Button) findViewById(R.id.clear);
-    public Button strum=(Button) findViewById(R.id.button);
-    public EditText strumpattern=(EditText) findViewById(R.id.StrumPattern);
- */
-
-
-/*
-Clear button code
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                strumpattern.setText(null);
-            }
-        });
-
- */
