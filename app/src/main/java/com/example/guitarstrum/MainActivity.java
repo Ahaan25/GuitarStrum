3b0a9c95ff8f014d146f725494422dd6bf19d63d
Ahaan25 <ahaan25092002@gmail.com>
2022-04-12 16:24:59 +0530

    Hehe

--- app/src/main/java/com/example/guitarstrum/MainActivity.java
+++ app/src/main/java/com/example/guitarstrum/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.guitarstrum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,36 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText strm;
+    Button addchords;
+    Button clear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addchords=findViewById(R.id.button);
+        addchords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChooseChords();
+            }
+        });
+
+        clear=findViewById(R.id.clear);
+        strm=findViewById(R.id.StrumPattern);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strm.setText(null);
+            }
+        });
+
+    }
+
+    public void openChooseChords(){
+        Intent intent=new Intent(this, ChooseChords.class);
+        startActivity(intent);
     }
 }
 
