848aacbf9c20ca9e3ce107c53f53a137ecb11815
Ahaan25 <ahaan25092002@gmail.com>
2022-04-12 15:29:02 +0530

    Hehe

--- app/src/main/java/com/example/guitarstrum/MainActivity.java
+++ app/src/main/java/com/example/guitarstrum/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.guitarstrum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
+
+/*
+Widgets declared
+    public Button up=(Button) findViewById(R.id.up);
+    public Button down=(Button) findViewById(R.id.down);
+    public Button slap=(Button) findViewById(R.id.slap);
+    public Button clear=(Button) findViewById(R.id.clear);
+    public Button strum=(Button) findViewById(R.id.button);
+    public EditText strumpattern=(EditText) findViewById(R.id.StrumPattern);
+ */
+
+
+/*
+Clear button code
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                strumpattern.setText(null);
+            }
+        });
+
+ */
