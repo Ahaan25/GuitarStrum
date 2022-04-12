df9868c9e06b983957f115bafc7cf264ac21c8d6
Ahaan25 <ahaan25092002@gmail.com>
2022-04-12 17:46:39 +0530

    Hehe

--- app/src/main/java/com/example/guitarstrum/ChooseChords.java
+++ app/src/main/java/com/example/guitarstrum/ChooseChords.java
@@ -3,12 +3,25 @@ package com.example.guitarstrum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseChords extends AppCompatActivity {
 
+    Button addchord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_chords);
+
+
+        addchord=findViewById(R.id.addchords);
+        addchord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
